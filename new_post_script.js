ab6ba5d89f796c096c0abda7909ce7bbd8804686
Nektaria Karagiannis <91200279+nikkikaragi@users.noreply.github.com>
2026-09-06 18:24:50 -0400

    cosmetic changes

--- new_post_script.js
+++ new_post_script.js
@@ -200,7 +200,7 @@ async function main() {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>NK Blog — ${title}</title>
+  <title>eatnektarines — ${title}</title>
   <link rel="stylesheet" href="../styles/fonts.css">
   <link rel="stylesheet" href="../styles/colors.css">
   <link rel="stylesheet" href="../styles/base.css">
